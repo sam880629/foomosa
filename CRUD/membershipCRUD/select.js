fd520a7d462323538c3d6c3f71fcbac6b8e7ef8f
Sam <x0710078@gmail.com>
2023-05-19 15:02:50 +0800

    更改我的評論最新時間順序

--- CRUD/membershipCRUD/select.js
+++ CRUD/membershipCRUD/select.js
@@ -25,9 +25,11 @@ router.get("/", function (req, res) {
 
     const sql3 = `SELECT * FROM history_text WHERE user_id = ${user_id} ORDER BY created_at DESC LIMIT 10`;
     
+    //--查詢 user 表
     const sql4 = `SELECT * FROM user WHERE user_id = ${user_id}`;
 
-    config.query(`${sql1}; ${sql2}; ${sql3}; ${sql4};`,
+    const sql5 = `SELECT u.user_name, u.user_avatar, s.shop_name, s.shop_logo_img, s.shop_preview_img, c.comment_text, user_point, c.shop_id, c.comment_favorite, DATE_FORMAT(c.created_at, '%Y-%m-%d %H:%i') created_at FROM user u JOIN comment c ON u.user_id = c.user_id JOIN shop s ON c.shop_id = s.shop_id WHERE u.user_id = 1 ORDER BY created_at DESC`;
+    config.query(`${sql1}; ${sql2}; ${sql3}; ${sql4}; ${sql5};`,
         (err, results, fields) => { //-- 結果儲存在 results 中
             // console.log(results);
             if (err) {
@@ -38,6 +40,7 @@ router.get("/", function (req, res) {
             }
             // console.log(results);
             // console.log(results[0]);
+            console.log(results[4][1].created_at);
             // console.log(results[0][0].user_point);
             // console.log(results[0][1].shop_preview_img);
             // console.log(results[0][0].shop_logo_img);
