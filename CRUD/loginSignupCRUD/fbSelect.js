fa641498499688ea287acccc082687a2bf79cf25
MSI\alylin <aly.chianing@gmail.com>
2023-05-19 15:35:27 +0800

    no message

--- CRUD/loginSignupCRUD/fbSelect.js
+++ CRUD/loginSignupCRUD/fbSelect.js
@@ -1,48 +1,50 @@
 const express = require('express');
-const api = express.Router();
+const app = express();
 
-api.post('/', express.urlencoded(), function (req, res) {
+app.post('/', express.urlencoded(), function (req, res) {
     // 連線資料庫
     const conn = require('../config.js');
-
-
-    console.log("data");
-    console.log(req.body.pId);
 
     const sqlSelect = "select * from user WHERE user_fb_id = ?;";
     conn.query(sqlSelect, // 先搜尋看看資料庫有沒有這個 fb_id
         [req.body.pId],
-        function (err, results, fields) {
+        async function (err, results, fields) {
             if (!err) {
                 if (results.length > 0) {
-                    req.session.uid = results[0].user_id; // 有的話，將 user_id 存進 session
-                    res.redirect('/index'); // 導回主頁
+                    req.session.uid = results[0].user_id;
+                    res.redirect('/index');
                 } else {
-                    let redirectUrl = createAccount(req.body.pAvatar);
-                    res.redirect(redirectUrl);
+                    let createId = await createFbAccount(req.body); 
+                    if (createId > 0) {
+                        req.session.uid = createId;
+                        res.redirect('/index');
+                    } else {
+                        res.redirect('/');
+                    }
                 }
             } else {
-                // console.log(err);
-                res.redirect('/'); // 404
+                console.log(err);
+                res.redirect('/');
             }
         })
 })
 
-function createAccount({ pId, pName, pAvatar, pEmail }) {
+function createFbAccount({ pId, pName, pAvatar, pEmail }) {
     const conn = require('../config.js'); // 連線資料庫
-    // console.log(pName);
+
     const sqlInsert = "INSERT INTO user(user_name, user_email, user_avatar, user_fb_id) VALUES (?, ?, ?, ?);";
-    conn.query(sqlInsert,  // 沒有的話，就建一筆新的 user
-        [pName, pEmail, pAvatar, pId],
-        function (err, results, fields) {
+
+    // 等待執行結果
+    return new Promise((resolve, reject) => {
+        conn.query(sqlInsert, [pName, pEmail, pAvatar, pId], function (err, results, fields) {
             if (!err) {
-                req.session.uid = results.insertId;
-                return '/index';
+                resolve(results.insertId);
             } else {
                 console.log(err);
-                return '/';
+                reject(err);
             }
-        })
+        });
+    });
 }
 
-module.exports = api;
+module.exports = app;
--- CRUD/loginSignupCRUD/googleCreate.js
+++ CRUD/loginSignupCRUD/googleCreate.js
@@ -8,24 +8,19 @@ app.post('/', express.urlencoded(), function (req, res) {
     const sqlSelect = "select * from user WHERE user_google_id = ?;";
     conn.query(sqlSelect, // 先搜尋看看資料庫有沒有這個 google_id
         [req.body.pId],
-        function (err, results, fields) {
+        async function (err, results, fields) {
             if (!err) {
                 if (results.length > 0) {
                     req.session.uid = results[0].user_id;
                     res.redirect('/index');
                 } else {
-                    const sqlInsert = "INSERT INTO user(user_name, user_email, user_avatar, user_google_id) VALUES (?, ?, ?, ?);";
-                    conn.query(sqlInsert,  // 沒有的話，就建一筆新的 user
-                        [req.body.pName, req.body.pEmail, req.body.pAvatar, req.body.pId],
-                        function (err, results, fields) {
-                            if (!err) {
-                                req.session.uid = results.insertId;
-                                res.redirect('/index');
-                            } else {
-                                console.log(err);
-                                res.redirect('/');
-                            }
-                        })
+                    let createId = await createGoogleAccount(req.body); 
+                    if (createId > 0) {
+                        req.session.uid = createId;
+                        res.redirect('/index');
+                    } else {
+                        res.redirect('/');
+                    }
                 }
             } else {
                 console.log(err);
@@ -33,5 +28,23 @@ app.post('/', express.urlencoded(), function (req, res) {
             }
         })
 })
+
+function createGoogleAccount({ pId, pName, pAvatar, pEmail }) {
+    const conn = require('../config.js'); // 連線資料庫
+
+    const sqlInsert = "INSERT INTO user(user_name, user_email, user_avatar, user_google_id) VALUES (?, ?, ?, ?);";
+
+    // 等待執行結果
+    return new Promise((resolve, reject) => {
+        conn.query(sqlInsert, [pName, pEmail, pAvatar, pId], function (err, results, fields) {
+            if (!err) {
+                resolve(results.insertId);
+            } else {
+                console.log(err);
+                reject(err);
+            }
+        });
+    });
+}
 
 module.exports = app;
