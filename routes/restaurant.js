4148cb92b517391a9184fa67eff7f45eb0f34888
DESKTOP-QJEM3AF\USER <l4371alk@gmail.com>
2023-05-20 22:30:20 +0800

    add modal

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -46,8 +46,8 @@ app.get('/:id', async function (req, res) { // 修改路由以接收 id 參數
         // console.log('我要看Current URL:', req.session.url);
         // console.log( '我要看uid');
         // console.log( req.session.uid);
-        // console.log( '我要看shopId');
-        // console.log( req.session.shopId);
+        console.log( '我要看shopId');
+        console.log( req.session.shopId);
         res.render('restaurant', {
             // shops: response.data, // 將整個結果集傳遞給 EJS 模板
             shop: response.data[0][0],
